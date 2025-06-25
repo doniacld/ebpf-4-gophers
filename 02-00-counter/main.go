8110f1b2dd7a46063defcf3509bf8b19d66c1374
Donia Chaiehloudj <donia.cld@isovalent.com>
2025-06-25 17:59:25 +0200

    doc: Update README and rename numbers
    Signed-off-by: Donia Chaiehloudj <donia.cld@isovalent.com>

---
+++ 02-00-counter/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/rlimit"
+)
+
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go counter counter.c
+
+func main() {
+	// remove resource limits for kernels <5.11
+	// ? why do we need to remove a limit, what is the limit
+	// and what is my kernel version?
+	err := rlimit.RemoveMemlock()
+	if err != nil {
+		log.Fatal("Removing memlock", err)
+	}
+
+	var objs counterObjects
+	if err := loadCounterObjects(&objs, nil); err != nil {
+		log.Fatal("Loading eBPF objects", err)
+	}
+	defer objs.Close()
+
+	ifname := "eth0"
+	iface, err := net.InterfaceByName(ifname)
+	if err != nil {
+		log.Fatalf("Getting interface %s: %s", ifname, err)
+	}
+
+	xdp, err := link.AttachXDP(link.XDPOptions{
+		Program:   objs.CountPackets,
+		Interface: iface.Index,
+	})
+	if err != nil {
+		log.Fatalf("Attaching XDP: %s", err)
+	}
+	defer xdp.Close()
+
+	log.Printf("Counting incoming packets on %s...", ifname)
+
+	// Periodically fetch the packet counter from PktCount,
+	// exit the program when interrupted.
+	tick := time.Tick(time.Second)
+	stop := make(chan os.Signal, 5)
+	signal.Notify(stop, os.Interrupt)
+	for {
+		select {
+		case <-tick:
+			var count uint64
+			err := objs.PktCount.Lookup(uint32(0), &count)
+			if err != nil {
+				log.Fatal("Map lookup:", err)
+			}
+			log.Printf("Received %d packets", count)
+		case <-stop:
+			log.Print("Received signal, exiting..")
+			return
+		}
+	}
+}
--- 02-counter/go.mod
+++
@@ -1,7 +0,0 @@
-module github.com/doniacld/lima/counter
-
-go 1.23.1
-
-require github.com/cilium/ebpf v0.17.1
-
-require golang.org/x/sys v0.26.0 // indirect
